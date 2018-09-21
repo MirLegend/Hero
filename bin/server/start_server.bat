e25ffc1265a518fc42c70133cdb8dfbe83419d38
MirLegend <zcg43951@126.com>
2018-09-21 17:59:04 +0800

    up

--- bin/server/start_server.bat
+++ bin/server/start_server.bat
@@ -7,5 +7,5 @@ start login.exe --cid=9001 --gus=11
 start dbmgr.exe --cid=9002 --gus=11
 start cellmgr.exe --cid=9003 --gus=11
 start basemgr.exe --cid=9004 --gus=11
-
+start baseapp.exe --cid=9005 --gus=11
 start cellapp.exe --cid=9006 --gus=11
